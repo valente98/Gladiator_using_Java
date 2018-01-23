147ac395890015a76eb74922d50643821083a4ea
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-23 15:35:32 -0600

    added all the files

---
+++ src/com/company/Gladiator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Gladiator {
+    public String name;
+    public Integer health;
+    public Integer rage;
+    public Integer damage_low;
+    public Integer damage_hi;
+
+    Gladiator(String name, Integer health, Integer rage, Integer damage_low, Integer damage_hi){
+        this.name = name;
+        this.health = health;
+        this.rage = rage;
+        this.damage_low = damage_low;
+        this.damage_hi = damage_hi;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO GLADIATOR WARS!");
+        System.out.println("Gladiator 1 has a low damage of 12 and a high damage of 20");
+        System.out.println("Gladiator 2 has a low damage of 12 and a high damage of 20");
+        System.out.println("CHOOSE WISELY....");
+        Scanner gladiator1 = new Scanner(System.in);
+        System.out.println("Gladiator one type in your name:");
+        Scanner gladiator2 = new Scanner(System.in);
+        System.out.println("Gladiator two type in your name:");
+
+        Gladiator gladiatorone = new Gladiator(""+gladiator1, 200, 0, 12, 20);
+        Gladiator gladiatortwo = new Gladiator(""+gladiator2, 200, 0, 5, 30);
+        while(true){
+
+        }
+    }
+}
