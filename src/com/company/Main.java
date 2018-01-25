bce7159bdebda720903c0b1a281201600cce77c9
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-25 13:14:49 -0600

    created a class that will interact with the user.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,15 @@ public class Main {
         System.out.println("Gladiator 1 has a low damage of 12 and a high damage of 20");
         System.out.println("Gladiator 2 has a low damage of 12 and a high damage of 20");
         System.out.println("CHOOSE WISELY....");
-        Scanner gladiator1 = new Scanner(System.in);
         System.out.println("Gladiator one type in your name:");
-        Scanner gladiator2 = new Scanner(System.in);
+        Scanner gladiator1 = new Scanner(System.in);
         System.out.println("Gladiator two type in your name:");
-
-        Gladiator gladiatorone = new Gladiator(""+gladiator1, 200, 0, 12, 20);
-        Gladiator gladiatortwo = new Gladiator(""+gladiator2, 200, 0, 5, 30);
+        Scanner gladiator2 = new Scanner(System.in);
+        Gladiator gladiatorone = new Gladiator("" + gladiator1, 200, 0, 12, 20);
+        Gladiator gladiatortwo = new Gladiator("" + gladiator2, 200, 0, 5, 30);
         while(true){
 
         }
     }
+
 }
