907ca907d26103b55f5ee7094eb0c8032631af8c
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-26 12:51:34 -0600

    created a class that will check the test for the gladiator.

---
+++ test/com/company/GladiatorTest.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GladiatorTest {
+
+    @Test
+    void attack() {
+    }
+
+    @Test
+    void passRage() {
+    }
+
+    @Test
+    void heal() {
+    }
+
+    @Test
+    void is_dead() {
+    }
+
+    @Test
+    void punch() {
+    }
+
+    @Test
+    void super_heal() {
+    }
+
+    @Test
+    void healer() {
+    }
+
+    @Test
+    void barbarian() {
+    }
+
+    @Test
+    void sword() {
+    }
+
+    @Test
+    void lance() {
+    }
+
+    @Test
+    void bow_and_arrow() {
+    }
+
+    @Test
+    void knife() {
+    }
+}
