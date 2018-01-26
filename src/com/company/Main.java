a57b759935b84304da2817d0bbefaeb588e6fd33
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-26 12:49:19 -0600

    added more statements then made it to where it would make the gladiators then it would run in a loop until one of the gladiators dies.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO GLADIATOR WARS!");
-        System.out.println("Gladiator 1 has a low damage of 12 and a high damage of 20");
-        System.out.println("Gladiator 2 has a low damage of 12 and a high damage of 20");
-        System.out.println("CHOOSE WISELY....");
+        Scanner glad = new Scanner(System.in);
+        GladiatorInteraction gladiator = new GladiatorInteraction();
+        gladiator.intro();
         System.out.println("Gladiator one type in your name:");
-        Scanner gladiator1 = new Scanner(System.in);
+        String glad1 = glad.nextLine();
         System.out.println("Gladiator two type in your name:");
-        Scanner gladiator2 = new Scanner(System.in);
-        Gladiator gladiatorone = new Gladiator("" + gladiator1, 200, 0, 12, 20);
-        Gladiator gladiatortwo = new Gladiator("" + gladiator2, 200, 0, 5, 30);
-        while(true){
+        String glad2 = glad.nextLine();
+        gladiator.gladiator_intro(glad1, glad2);
+        Gladiator gladiatorone = new Gladiator("" + glad1, 200, 0, 12, 20);
+        Gladiator gladiatortwo = new Gladiator("" + glad2, 200, 0, 5, 30);
+        while (true) {
+                gladiator.Gladiator_choices(glad1);
+                String choice = glad.nextLine();
+                gladiator.Gladiators_choice(choice, gladiatorone, gladiatortwo, glad1, glad2);
+                gladiator.Gladiator_info(gladiatorone);
+                gladiator.Gladiator_info(gladiatortwo);
+                gladiator.Gladiator_choices(glad2);
+                String choices = glad.nextLine();
+                gladiator.Gladiators_choice(choices, gladiatortwo, gladiatorone, glad2, glad1);
+                gladiator.Gladiator_info(gladiatorone);
+                gladiator.Gladiator_info(gladiatortwo);
+            if (gladiatorone.is_dead() || gladiatortwo.is_dead()) {
+                if (!gladiatorone.is_dead()){
+                    System.out.println("Gladiator " + glad2 + "! WINS!");
+                    break;
+                } else if(!gladiatortwo.is_dead()){
+                    System.out.println("Gladiator " + glad2 + "! WINS!");
+                    break;
+                }
+            }
 
         }
     }
-
 }
